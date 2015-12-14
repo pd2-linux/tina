591af3c96e0811b0677e33770437861bd50d83fe
huzhen <huzhen@allwinnertech.com>
2015-12-14 16:08:22 +0800

    rftest: add wifi/bt rf test tools for ap6212
        1. add wifi_test.sh for wifi rf test
        2. add bt_test.sh for bt rf test

---
+++ package/utils/rftest/wifi_test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+insmod /lib/modules/3.4.39/bcmdhd.ko iface_name=wlan0 firmware_path=/etc/rftest/fw_bcm43438a0_mfg.bin nvram_path=/lib/firmware/ap6212/nvram_ap6212.txt
+sleep 2
+ifconfig wlan0 up
+sleep 1
+./wl ver
