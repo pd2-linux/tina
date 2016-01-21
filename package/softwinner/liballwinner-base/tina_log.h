d5eea7dbacbbf4fa572824cc977e3cb78bcc8df3
Heweihong <heweihong@allwinnertech.com>
2016-01-21 14:13:24 +0800

    liballwinner-base:     update: tina_log.h     add: thread.h locker.h taskbase.h
    Signed-off-by: Heweihong <heweihong@allwinnertech.com>

--- package/softwinner/liballwinner-base/tina_log.h
+++ package/softwinner/liballwinner-base/tina_log.h
@@ -4,17 +4,17 @@
 #include <syslog.h>
 
 
-#define OPTION_LOG_LEVEL_CLOSE      0
-#define OPTION_LOG_LEVEL_DEBUG      1
-#define OPTION_LOG_LEVEL_WARNING    2
-#define OPTION_LOG_LEVEL_DEFAULT    3
-#define OPTION_LOG_LEVEL_DETAIL     4
+#define OPTION_TLOG_LEVEL_CLOSE      0
+#define OPTION_TLOG_LEVEL_DEBUG      1
+#define OPTION_TLOG_LEVEL_WARNING    2
+#define OPTION_TLOG_LEVEL_DEFAULT    3
+#define OPTION_TLOG_LEVEL_DETAIL     4
 
-#define LOG_LEVEL_ERROR     "E/"
-#define LOG_LEVEL_WARNING   "W/"
-#define LOG_LEVEL_INFO      "I/"
-#define LOG_LEVEL_VERBOSE   "V/"
-#define LOG_LEVEL_DEBUG     "D/"
+#define TLOG_LEVEL_ERROR     "E/"
+#define TLOG_LEVEL_WARNING   "W/"
+#define TLOG_LEVEL_INFO      "I/"
+#define TLOG_LEVEL_VERBOSE   "V/"
+#define TLOG_LEVEL_DEBUG     "D/"
 
 #ifndef TAG
 #define TAG "Tina-default"
@@ -24,50 +24,50 @@
 #define LOG_TAG "["TAG"]"
 #endif
 
-#ifndef CONFIG_LOG_LEVEL
-#define CONFIG_LOG_LEVEL OPTION_LOG_LEVEL_DEFAULT
+#ifndef CONFIG_TLOG_LEVEL
+#define CONFIG_TLOG_LEVEL OPTION_TLOG_LEVEL_DEFAULT
 #endif
 
 #define TLOG(systag, level, fmt, arg...)  \
         syslog(systag, "%s:%s <%s:%u>: "fmt"\n", level, LOG_TAG, __FUNCTION__, __LINE__, ##arg)
 
 
-#define TLOGE(fmt, arg...) TLOG(LOG_ERR,LOG_LEVEL_ERROR, fmt, ##arg)
+#define TLOGE(fmt, arg...) TLOG(LOG_ERR,TLOG_LEVEL_ERROR, fmt, ##arg)
 
-#if CONFIG_LOG_LEVEL == OPTION_LOG_LEVEL_CLOSE
+#if CONFIG_TLOG_LEVEL == OPTION_TLOG_LEVEL_CLOSE
 
 #define TLOGD(fmt, arg...)
 #define TLOGW(fmt, arg...)
 #define TLOGI(fmt, arg...)
 #define TLOGV(fmt, arg...)
 
-#elif CONFIG_LOG_LEVEL == OPTION_LOG_LEVEL_DEBUG
+#elif CONFIG_TLOG_LEVEL == OPTION_TLOG_LEVEL_DEBUG
 
-#define TLOGD(fmt, arg...) TLOG(LOG_DEBUG,LOG_LEVEL_DEBUG, fmt, ##arg)
+#define TLOGD(fmt, arg...) TLOG(LOG_DEBUG,TLOG_LEVEL_DEBUG, fmt, ##arg)
 #define TLOGW(fmt, arg...)
 #define TLOGI(fmt, arg...)
 #define TLOGV(fmt, arg...)
 
-#elif CONFIG_LOG_LEVEL == OPTION_LOG_LEVEL_WARNING
+#elif CONFIG_TLOG_LEVEL == OPTION_TLOG_LEVEL_WARNING
 
-#define TLOGD(fmt, arg...) TLOG(LOG_DEBUG,LOG_LEVEL_DEBUG, fmt, ##arg)
-#define TLOGW(fmt, arg...) TLOG(LOG_WARNING,LOG_LEVEL_WARNING, fmt, ##arg)
+#define TLOGD(fmt, arg...) TLOG(LOG_DEBUG,TLOG_LEVEL_DEBUG, fmt, ##arg)
+#define TLOGW(fmt, arg...) TLOG(LOG_WARNING,TLOG_LEVEL_WARNING, fmt, ##arg)
 #define TLOGI(fmt, arg...)
 #define TLOGV(fmt, arg...)
 
-#elif CONFIG_LOG_LEVEL == OPTION_LOG_LEVEL_DEFAULT
+#elif CONFIG_TLOG_LEVEL == OPTION_TLOG_LEVEL_DEFAULT
 
-#define TLOGD(fmt, arg...) TLOG(LOG_DEBUG,LOG_LEVEL_DEBUG, fmt, ##arg)
-#define TLOGW(fmt, arg...) TLOG(LOG_WARNING,LOG_LEVEL_WARNING, fmt, ##arg)
-#define TLOGI(fmt, arg...) TLOG(LOG_INFO,LOG_LEVEL_INFO, fmt, ##arg)
+#define TLOGD(fmt, arg...) TLOG(LOG_DEBUG,TLOG_LEVEL_DEBUG, fmt, ##arg)
+#define TLOGW(fmt, arg...) TLOG(LOG_WARNING,TLOG_LEVEL_WARNING, fmt, ##arg)
+#define TLOGI(fmt, arg...) TLOG(LOG_INFO,TLOG_LEVEL_INFO, fmt, ##arg)
 #define TLOGV(fmt, arg...)
 
-#elif CONFIG_LOG_LEVEL == OPTION_LOG_LEVEL_DETAIL
+#elif CONFIG_TLOG_LEVEL == OPTION_TLOG_LEVEL_DETAIL
 
-#define TLOGD(fmt, arg...) TLOG(LOG_DEBUG,LOG_LEVEL_DEBUG, fmt, ##arg)
-#define TLOGW(fmt, arg...) TLOG(LOG_WARNING,LOG_LEVEL_WARNING, fmt, ##arg)
-#define TLOGI(fmt, arg...) TLOG(LOG_INFO,LOG_LEVEL_INFO, fmt, ##arg)
-#define TLOGV(fmt, arg...) TLOG(LOG_INFO,LOG_LEVEL_VERBOSE, fmt, ##arg)
+#define TLOGD(fmt, arg...) TLOG(LOG_DEBUG,TLOG_LEVEL_DEBUG, fmt, ##arg)
+#define TLOGW(fmt, arg...) TLOG(LOG_WARNING,TLOG_LEVEL_WARNING, fmt, ##arg)
+#define TLOGI(fmt, arg...) TLOG(LOG_INFO,TLOG_LEVEL_INFO, fmt, ##arg)
+#define TLOGV(fmt, arg...) TLOG(LOG_INFO,TLOG_LEVEL_VERBOSE, fmt, ##arg)
 
 #endif
 
